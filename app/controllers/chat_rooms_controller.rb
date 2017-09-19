bf63dd25fc048e05002f966f2da549a15280df09
Nicolas Sitter <nicolas.sitternolleau@gmail.com>
2017-09-19 14:08:32 +0200

    Add create message to continue chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -10,6 +10,7 @@ class ChatRoomsController < ApplicationController
   # GET /chat_rooms/1
   # GET /chat_rooms/1.json
   def show
+    @chat_room = ChatRoom.includes(messages: :user).find(params[:id])
   end
 
   # GET /chat_rooms/new
