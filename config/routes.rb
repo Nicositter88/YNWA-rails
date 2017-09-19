45401686988695cd2a2564aa1dfcdbc74519b442
Nicolas Sitter <nicolas.sitternolleau@gmail.com>
2017-09-19 12:43:59 +0200

    Add some chat rooms for seed / migrate messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :chat_rooms
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chat_rooms, only: [ :show ] do
+    resources :messages, only: [ :create ]
+  end
+
+  mount ActionCable.server => "/cable"
 end
